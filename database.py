ceec5ee259feb3358d8117b130ddad741c9f7102
merwinfattah <18219019@std.stei.itb.ac.id>
2022-06-06 13:13:14 +0700

    update database and main

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 
 def open_connection():
-    cnx = mysql.connector.connect(user='root', password='#5O<##l7n-1R_IO<', host='34.132.141.97', 
+    cnx = mysql.connector.connect(user='root', password='\H5:fYy&%k"y)bX[', host='34.101.157.254', 
                               database='waralobby_db')
 
     return cnx
--- main.py
+++ main.py
@@ -39,6 +39,9 @@ def get_AllFranchisor():
     except Exception as e:
         print(e)
 
+@
+
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
